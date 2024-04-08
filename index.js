5166baa89db3c607264f1345fdb78cda02f6e41e
Izabela B <izabester@gmail.com>
2024-04-08 11:25:28 +0100

    exercises 3.1-3.6

---
+++ index.js
@@ -0,0 +1,106 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebookEntries = 
+[
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Welocme to the Phonebook!</h1>')
+})
+  
+app.get('/api/persons', (request, response) => {
+response.json(phonebookEntries)
+})
+
+app.get('/api/info', (request, response) => {
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'long'
+    }
+    const formatter = new Intl.DateTimeFormat('en-us', options)
+    const formattedDate = formatter.format(new Date()) 
+    const message = `Phonebook has info for ${phonebookEntries.length} people`
+    response.send(`${message}<br>${formattedDate}`)
+})
+
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = phonebookEntries.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+})
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    phonebookEntries = phonebookEntries.filter(person => person.id !== id)
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = phonebookEntries.length > 0
+      ? Math.max(...phonebookEntries.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+  if (!body.name || !body.number) {
+    return response.status(400).json({ 
+      error: 'request is malformed' 
+    })
+  }
+  const entryAlreadyExists = phonebookEntries.find(person => person.name == body.name)
+  if (entryAlreadyExists) {
+    return response.status(400).json({ 
+        error: 'name must be unique' 
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+  
+  phonebookEntries = phonebookEntries.concat(person)
+  response.json(person)
+  })
+  
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
