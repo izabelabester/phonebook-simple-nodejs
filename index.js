11ea10a37fe8ed50ef86c7964c122c83e7f71399
Izabela B <izabester@gmail.com>
2024-04-09 17:18:42 +0100

    part 3c final exercises

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 // app.use(morgan('tiny'));
-morgan.token('body', req => {
-    return JSON.stringify(req.body)
-})
+morgan.token('body', req => { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
  
 const requestLogger = (request, response, next) => {
@@ -19,21 +17,120 @@ const requestLogger = (request, response, next) => {
     console.log('Body:  ', request.body)
     console.log('---')
     next()
-  }
+}
+
 app.use(requestLogger) 
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
-app.get('/api/persons', (request, response) => {
-    console.log("app.get triggerred")
-// response.json(phonebookEntries)
-    Person.find({}).then(persons => {
-        console.log("found persons: ", persons)
+app.get('/api/persons', (request, response, next) => {
+    Person.find({})
+    .then(persons => {
         response.json(persons)
     })
+    .catch(error => next(error))
 })
+
+app.get('/api/info', (request, response) => {
+    const options = {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        timeZoneName: 'long'
+    }
+    const formatter = new Intl.DateTimeFormat('en-us', options)
+    const formattedDate = formatter.format(new Date()) 
+    Person.find({})
+    .then(persons => {
+        const message = `Phonebook has info for ${persons.length} people`
+        response.send(`${message}<br>${formattedDate}`)
+})
+.catch(error => next(error))  
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+    //using a hardcoded list of entries:
+    // const id = Number(request.params.id)
+    // const person = phonebookEntries.find(person => person.id === id)
+    // if (person) {
+    //     response.json(person)
+    //   } else {
+    //     response.status(404).end()
+    //   }
+
+      Person.findById(request.params.id)
+      .then(person => {
+        response.send(JSON.parse(JSON.stringify(person))) //format so that every param is displayed in its own line
+      })
+      .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+  
+    if (!body.name || !body.number) {
+        return response.status(400).json({ 
+            error: 'request is malformed' 
+        })
+    }
+    
+    const person = new Person({
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * 100)
+    })
+  
+    person.save().then(savedPerson => {
+      response.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+  
+    Person.findByIdAndUpdate(request.params.id, {name: body.name, number: body.number}, { new: true })
+      .then(updatedPersonRecord => {
+        response.json(updatedPersonRecord)
+      })
+      .catch(error => next(error))
+  })
+
+app.use(unknownEndpoint)
+  
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
+
+//Using a hardcoded list of entries:
 
 // app.get('/api/info', (request, response) => {
 //     const options = {
@@ -50,27 +147,6 @@ app.get('/api/persons', (request, response) => {
 //     const formattedDate = formatter.format(new Date()) 
 //     const message = `Phonebook has info for ${phonebookEntries.length} people`
 //     response.send(`${message}<br>${formattedDate}`)
-// })
-
-
-app.get('/api/persons/:id', (request, response) => {
-    // const id = Number(request.params.id)
-    // const person = phonebookEntries.find(person => person.id === id)
-    // if (person) {
-    //     response.json(person)
-    //   } else {
-    //     response.status(404).end()
-    //   }
-
-      Person.findById(request.params.id).then(person => {
-        response.json(person)
-      })
-})
-
-// app.get('/api/notes', (request, response) => {
-//     Note.find({}).then(notes => {
-//       response.json(notes)
-//     })
 // })
 
 // app.delete('/api/persons/:id', (request, response) => {
@@ -110,28 +186,3 @@ app.get('/api/persons/:id', (request, response) => {
 //   phonebookEntries = phonebookEntries.concat(person)
 //   response.json(person)
 // })
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body
-  
-    if (!body.name || !body.number) {
-        return response.status(400).json({ 
-            error: 'request is malformed' 
-        })
-    }
-    
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-        id: Math.floor(Math.random() * 100)
-    })
-  
-    person.save().then(savedPerson => {
-      response.json(savedPerson)
-    })
-})
-  
-const PORT = 3001
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
-})
--- mongo.js
+++ mongo.js
@@ -1,58 +1,58 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+// import { createRequire } from 'module';
+// const require = createRequire(import.meta.url);
 
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
+// if (process.argv.length<3) {
+//   console.log('give password as argument')
+//   process.exit(1)
+// }
 
-const password = process.argv[2]
-const name = process.argv[3]
-const number = process.argv[4]
+// const password = process.argv[2]
+// const name = process.argv[3]
+// const number = process.argv[4]
 
-const url =
-  `mongodb+srv://izabester1:${password}@cluster0.o4bh3qk.mongodb.net/phonebook?retryWrites=true&w=majority`
+// const url =
+//   `mongodb+srv://izabester1:${password}@cluster0.o4bh3qk.mongodb.net/phonebook?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery',false)
+// mongoose.set('strictQuery',false)
 
-mongoose.connect(url)
+// mongoose.connect(url)
 
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-  id: Number
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-if (name) {
-  const person = new Person({
-    name: name,
-    number: number,
-    id: Math.floor(Math.random() * 100)
-  })
-
-  person.save().then(result => {
-    console.log(`added ${person.name} number ${person.number} to phonebook!`)
-    mongoose.connection.close()
-  })
-
-} else {
-    Person.find({}).then(result => {
-      console.log("phonebook:")
-    result.forEach(person => {
-      console.log(`${person.name} ${person.number}`)
-    })
-    mongoose.connection.close()
-  })
-}
-
-// noteSchema.set('toJSON', {
-//   transform: (document, returnedObject) => {
-//     returnedObject.id = returnedObject._id.toString()
-//     delete returnedObject._id
-//     delete returnedObject.__v
-//   }
+// const personSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+//   id: Number
 // })
+
+// const Person = mongoose.model('Person', personSchema)
+
+// if (name) {
+//   const person = new Person({
+//     name: name,
+//     number: number,
+//     id: Math.floor(Math.random() * 100)
+//   })
+
+//   person.save().then(result => {
+//     console.log(`added ${person.name} number ${person.number} to phonebook!`)
+//     mongoose.connection.close()
+//   })
+
+// } else {
+//     Person.find({}).then(result => {
+//       console.log("phonebook:")
+//     result.forEach(person => {
+//       console.log(`${person.name} ${person.number}`)
+//     })
+//     mongoose.connection.close()
+//   })
+// }
+
+// // noteSchema.set('toJSON', {
+// //   transform: (document, returnedObject) => {
+// //     returnedObject.id = returnedObject._id.toString()
+// //     delete returnedObject._id
+// //     delete returnedObject.__v
+// //   }
+// // })
